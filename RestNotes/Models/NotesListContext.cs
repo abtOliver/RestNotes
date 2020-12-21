418829d8ee2a6cd36ad892462c1413f6e684dc35
Oliver G. Abt <abto@lars-net.de>
2020-12-21 23:31:27 +0100

    Basisimplementation eines Memory basierten Notiz Speichers

---
+++ RestNotes/Models/NotesListContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestNotes.Models
+{
+    public class NotesListContext
+    {
+        readonly List<Note> notes = new List<Note>();
+
+        public IEnumerable<Note> ListAllNotes()
+        {
+            return notes;
+        }
+    }
+}
---
+++ RestNotesTests/Tests/Models/NotesListContextTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestNotes.Models;
+using System;
+using System.Linq;
+
+namespace RestNotesTests.Tests.Models
+{
+    [TestClass()]
+    public class NotesListContextTests
+    {
+        [TestMethod()]
+        public void GivenNewNotesListContext_GetNotes_ShouldReturnEmptyCollection()
+        {
+            var expectedEmpty = Array.Empty<Note>();
+
+            var context = new NotesListContext();
+
+            var actualNotes = context.ListAllNotes();
+
+            CollectionAssert.AreEqual(expectedEmpty, actualNotes.ToList());
+        }
+    }
+}
